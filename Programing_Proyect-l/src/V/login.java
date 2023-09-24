c225a8d560eac442e08fbbecf0b7fa4917c06906
Jefry Morera <jefrymoreraespinoza09@gmail.com>
2023-09-23 18:46:40 -0600

    Login created

---
+++ Programing_Proyect-l/src/V/login.java
@@ -0,0 +1,301 @@
+package V;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author jefry
+ */
+public class login extends javax.swing.JFrame {
+
+    private int xMouse;
+    private int yMouse;
+
+    public login() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollBar1 = new javax.swing.JScrollBar();
+        PNMain = new javax.swing.JPanel();
+        lblTitle = new javax.swing.JLabel();
+        lblCity = new javax.swing.JLabel();
+        lblLogin = new javax.swing.JLabel();
+        lblPassword = new javax.swing.JLabel();
+        spPassword = new javax.swing.JSeparator();
+        lblUser = new javax.swing.JLabel();
+        txtUser = new javax.swing.JTextField();
+        spUser = new javax.swing.JSeparator();
+        psPassword = new javax.swing.JPasswordField();
+        pnButtonEnter = new javax.swing.JPanel();
+        lblButtonEnter = new javax.swing.JLabel();
+        pnBar = new javax.swing.JPanel();
+        pnButtonExit = new javax.swing.JPanel();
+        lblExit = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setUndecorated(true);
+        setResizable(false);
+
+        PNMain.setBackground(new java.awt.Color(255, 255, 255));
+        PNMain.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblTitle.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(0, 0, 0));
+        lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblTitle.setText("Titulo");
+        PNMain.add(lblTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 200, 250, -1));
+
+        lblCity.setIcon(new javax.swing.ImageIcon(getClass().getResource("/V/Images/ciudad.jpg"))); // NOI18N
+        PNMain.add(lblCity, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 0, 260, 510));
+
+        lblLogin.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        lblLogin.setForeground(new java.awt.Color(0, 0, 0));
+        lblLogin.setText("INICIAR SESIÓN");
+        PNMain.add(lblLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, -1, 40));
+
+        lblPassword.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblPassword.setForeground(new java.awt.Color(0, 0, 0));
+        lblPassword.setText("Contraseña");
+        PNMain.add(lblPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 110, 30));
+        PNMain.add(spPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 340, 470, 20));
+
+        lblUser.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblUser.setForeground(new java.awt.Color(0, 0, 0));
+        lblUser.setText("USUARIO");
+        PNMain.add(lblUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, 90, 30));
+
+        txtUser.setBackground(new java.awt.Color(255, 255, 255));
+        txtUser.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        txtUser.setForeground(new java.awt.Color(204, 204, 204));
+        txtUser.setText("Ingrese su nombre de usuario");
+        txtUser.setBorder(null);
+        txtUser.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txtUserMousePressed(evt);
+            }
+        });
+        txtUser.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUserActionPerformed(evt);
+            }
+        });
+        PNMain.add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 470, 30));
+        PNMain.add(spUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 470, 10));
+
+        psPassword.setBackground(new java.awt.Color(255, 255, 255));
+        psPassword.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        psPassword.setForeground(new java.awt.Color(204, 204, 204));
+        psPassword.setText("********");
+        psPassword.setBorder(null);
+        psPassword.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                psPasswordMousePressed(evt);
+            }
+        });
+        PNMain.add(psPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 310, 470, 30));
+
+        pnButtonEnter.setBackground(new java.awt.Color(0, 134, 190));
+
+        lblButtonEnter.setBackground(new java.awt.Color(0, 100, 190));
+        lblButtonEnter.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        lblButtonEnter.setForeground(new java.awt.Color(255, 255, 255));
+        lblButtonEnter.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblButtonEnter.setText("Entrar");
+        lblButtonEnter.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblButtonEnter.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblButtonEnter.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblButtonEnterMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblButtonEnterMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblButtonEnterMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout pnButtonEnterLayout = new javax.swing.GroupLayout(pnButtonEnter);
+        pnButtonEnter.setLayout(pnButtonEnterLayout);
+        pnButtonEnterLayout.setHorizontalGroup(
+            pnButtonEnterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblButtonEnter, javax.swing.GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+        );
+        pnButtonEnterLayout.setVerticalGroup(
+            pnButtonEnterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblButtonEnter, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+        );
+
+        PNMain.add(pnButtonEnter, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 400, 110, 50));
+
+        pnBar.setBackground(new java.awt.Color(255, 255, 255));
+        pnBar.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                pnBarMouseDragged(evt);
+            }
+        });
+        pnBar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                pnBarMouseEntered(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                pnBarMousePressed(evt);
+            }
+        });
+
+        pnButtonExit.setBackground(new java.awt.Color(255, 255, 255));
+
+        lblExit.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        lblExit.setForeground(new java.awt.Color(0, 0, 0));
+        lblExit.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblExit.setText("X");
+        lblExit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblExit.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblExitMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblExitMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblExitMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout pnButtonExitLayout = new javax.swing.GroupLayout(pnButtonExit);
+        pnButtonExit.setLayout(pnButtonExitLayout);
+        pnButtonExitLayout.setHorizontalGroup(
+            pnButtonExitLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblExit, javax.swing.GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+        );
+        pnButtonExitLayout.setVerticalGroup(
+            pnButtonExitLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblExit, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout pnBarLayout = new javax.swing.GroupLayout(pnBar);
+        pnBar.setLayout(pnBarLayout);
+        pnBarLayout.setHorizontalGroup(
+            pnBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnBarLayout.createSequentialGroup()
+                .addComponent(pnButtonExit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 779, Short.MAX_VALUE))
+        );
+        pnBarLayout.setVerticalGroup(
+            pnBarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnButtonExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        PNMain.add(pnBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 40));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(PNMain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(PNMain, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void pnBarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnBarMouseEntered
+
+    }//GEN-LAST:event_pnBarMouseEntered
+
+    private void pnBarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnBarMousePressed
+
+        xMouse = evt.getX();
+        yMouse = evt.getY();
+    }//GEN-LAST:event_pnBarMousePressed
+
+    private void pnBarMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnBarMouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - xMouse, y - yMouse);
+    }//GEN-LAST:event_pnBarMouseDragged
+
+    private void lblExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblExitMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_lblExitMouseClicked
+
+    private void lblExitMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblExitMouseEntered
+        pnButtonExit.setBackground(Color.red);
+        lblExit.setForeground(Color.white);
+    }//GEN-LAST:event_lblExitMouseEntered
+
+    private void lblExitMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblExitMouseExited
+        pnButtonExit.setBackground(Color.white);
+        lblExit.setForeground(Color.black);
+    }//GEN-LAST:event_lblExitMouseExited
+
+    private void lblButtonEnterMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblButtonEnterMouseEntered
+        pnButtonEnter.setBackground(new Color(0, 156, 233));
+    }//GEN-LAST:event_lblButtonEnterMouseEntered
+
+    private void lblButtonEnterMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblButtonEnterMouseExited
+        pnButtonEnter.setBackground(new Color(0, 134, 190));
+    }//GEN-LAST:event_lblButtonEnterMouseExited
+
+    private void txtUserMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtUserMousePressed
+        if (txtUser.getText().equals("Ingrese su nombre de usuario")) {
+            txtUser.setText("");
+            txtUser.setForeground(Color.BLACK);
+        }
+        if (String.valueOf(psPassword.getPassword()).isEmpty()) {
+            psPassword.setText("********");
+            psPassword.setForeground(Color.GRAY);
+        }
+
+    }//GEN-LAST:event_txtUserMousePressed
+
+    private void psPasswordMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_psPasswordMousePressed
+        if (String.valueOf(psPassword.getPassword()).equals("********")) {
+            psPassword.setText("");
+            psPassword.setForeground(Color.BLACK);
+        }
+        if (txtUser.getText().isEmpty()) {
+            txtUser.setText("Ingrese su nombre de usuario");
+            txtUser.setForeground(Color.GRAY);
+        }
+
+
+    }//GEN-LAST:event_psPasswordMousePressed
+
+    private void lblButtonEnterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblButtonEnterMouseClicked
+
+    }//GEN-LAST:event_lblButtonEnterMouseClicked
+
+    private void txtUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUserActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUserActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PNMain;
+    private javax.swing.JScrollBar jScrollBar1;
+    private javax.swing.JLabel lblButtonEnter;
+    private javax.swing.JLabel lblCity;
+    private javax.swing.JLabel lblExit;
+    private javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblPassword;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JLabel lblUser;
+    private javax.swing.JPanel pnBar;
+    private javax.swing.JPanel pnButtonEnter;
+    private javax.swing.JPanel pnButtonExit;
+    private javax.swing.JPasswordField psPassword;
+    private javax.swing.JSeparator spPassword;
+    private javax.swing.JSeparator spUser;
+    private javax.swing.JTextField txtUser;
+    // End of variables declaration//GEN-END:variables
+}
